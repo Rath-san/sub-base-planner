38a6b74f135cd6cf34e475fec629fe849ffbbbeb
Rath-san <rim.kozlowski@gmail.com>
2018-02-22 15:00:47 +0100

    anim test

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -4,11 +4,31 @@ import { Item } from './item';
 import { Material } from './material';
 import { log } from 'util';
 
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+
 
 @Component({
   selector: 'app-main-content',
   templateUrl: './main-content.component.html',
-  styleUrls: ['./main-content.component.sass']
+  styleUrls: ['./main-content.component.sass'],
+  animations: [
+    trigger('pop', [
+      state('in', style({transform: 'translateX(0)'})),
+      transition('void => *', [
+        style({transform: 'translateX(-100%)'}),
+        animate(100)
+      ]),
+      transition('* => void', [
+        animate(100, style({transform: 'translateX(100%)'}))
+      ])
+    ])
+  ]
 })
 export class MainContentComponent implements OnInit {
 
@@ -24,9 +44,9 @@ export class MainContentComponent implements OnInit {
       },
       {
         raw: [
-          {name: 'a1', url: 'https://picsum.photos/200/200/?random', count: 3},
-          {name: 'r2', url: 'https://picsum.photos/200/200/?random', count: 2},
-          {name: 'r3', url: 'https://picsum.photos/200/200/?random', count: 1}
+          { name: 'a1', url: 'https://picsum.photos/200/200/?random', count: 3 },
+          { name: 'r2', url: 'https://picsum.photos/200/200/?random', count: 2 },
+          { name: 'r3', url: 'https://picsum.photos/200/200/?random', count: 1 }
         ],
         basic: [],
         advanced: []
@@ -79,12 +99,12 @@ export class MainContentComponent implements OnInit {
     basic: Material[],
     advanced: Material[]
   } = {
-    raw: [],
-    basic: [],
-    advanced: []
-  };
+      raw: [],
+      basic: [],
+      advanced: []
+    };
 
-  constructor(private ms: MainService) {}
+  constructor(private ms: MainService) { }
 
   ngOnInit() {
 
@@ -96,15 +116,18 @@ export class MainContentComponent implements OnInit {
   }
 
   removeFromBar(i) {
-    const index = this.itemsInBar.indexOf(i);
-    this.itemsInBar.splice(index, 1);
-    this.removeMaterial(i);
+    // console.log(this.itemsInBar.indexOf(i));
+    console.log(i);
+    
+    
+    // const index = this.itemsInBar.indexOf(i);
+    // this.itemsInBar.splice(index, 1);
+    // this.removeMaterial(i);
   }
 
   // create new material
   createMaterial(i: Item) {
     i.getMaterialsRaw().forEach((v: Material) => {
-
       // check if material is alrdy in raw return similar element
       const r = this.materialsInBom.raw.find(element => v.name === element.name);
       if (r) {
@@ -117,40 +140,31 @@ export class MainContentComponent implements OnInit {
       }
 
     });
-   }
+  }
 
-   removeMaterial(i: Item) {
-     i.getMaterialsRaw().forEach((v: Material) => {
-       const r = this.materialsInBom.raw.find(element => v.name === element.name);
-       if (r) {
-         // subtract from material count
-         r.count -= v.count;
-         if (r.count === 0) {
-           console.log();
-           const index = this.materialsInBom.raw.indexOf(r);
-           this.materialsInBom.raw.splice(index, 1);
-          //  this.materialsInBom.raw.map(e => {
-          //    console.log(e);
-          //   }).indexOf(r);
-          //  splice(index, 1);
-         }
-       }
-      //  else {
-      //    // add new material
-      //    const m = new Material(v.name, v.url, v.count);
-      //    this.materialsInBom.raw.push(m);
-      //  }
-     })
-   }
+  removeMaterial(i: Item) {
+    //  loop trough type i
+    i.getMaterialsRaw().forEach((v: Material) => {
+      // check if material is alrdy in raw return similar element
+      const r = this.materialsInBom.raw.find(element => v.name === element.name);
+      //  if is duplicate
+      if (r) {
+        // subtract from material count
+        r.count -= v.count;
+        //  if material in bom === 0 remove him
+        if (r.count === 0) {
+          console.log();
+          const index = this.materialsInBom.raw.indexOf(r);
+          this.materialsInBom.raw.splice(index, 1);
+        }
+      }
+    });
+  }
 
-   findSimilar(array: object[], prop, item: Object) {
-     const r = array.find((x, i) => {
-       console.log(i);
-       return x[prop] === item[prop];
-      });
-   }
-
-
-// this.materialsInBom.raw.find(x => x.name === v.name
-
+  findSimilar(array: object[], prop, item: Object) {
+    const r = array.find((x, i) => {
+      console.log(i);
+      return x[prop] === item[prop];
+    });
+  }
 }
